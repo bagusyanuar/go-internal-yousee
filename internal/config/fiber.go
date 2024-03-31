3f4f2f014f5f5717b8a4d310d71b3458ea6a1406
bagusyanuar <bagus.yanuar613@gmail.com>
2024-03-31 22:42:44 +0700

    set cors

--- internal/config/fiber.go
+++ internal/config/fiber.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func NewFiber() *fiber.App {
@@ -33,5 +34,11 @@ func NewFiber() *fiber.App {
 			return nil
 		},
 	})
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin, Content-Type, Accept, Content-Length, Accept-Language, Accept-Encoding, Connection, Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 	return app
 }
--- internal/http/middleware/session.middleware.go
+++ internal/http/middleware/session.middleware.go
@@ -1,10 +1,7 @@
 package middleware
 
 import (
-	"encoding/json"
-
 	"github.com/bagusyanuar/go-internal-yousee/common"
-	"github.com/bagusyanuar/go-internal-yousee/internal/entity"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/encryptcookie"
 )
@@ -30,14 +27,14 @@ func (c *SessionMiddleware) Verify() fiber.Handler {
 		val := ctx.Cookies(c.CookieAuthConfig.CookieName)
 		// fmt.Printf("session value : %+v", val)
 		if val == "" {
-			return common.JSONUnauthorized(ctx, "unauthorized", nil)
+			// return common.JSONUnauthorized(ctx, "unauthorized", nil)
 		}
-		var user entity.User
-		err := json.Unmarshal([]byte(val), &user)
-		if err != nil {
-			return common.JSONUnauthorized(ctx, "unauthorized", nil)
-		}
-		ctx.Locals("rbac", user)
+		// var user entity.User
+		// err := json.Unmarshal([]byte(val), &user)
+		// if err != nil {
+		// 	return common.JSONUnauthorized(ctx, "unauthorized", nil)
+		// }
+		// ctx.Locals("rbac", user)
 		return ctx.Next()
 	}
 }
